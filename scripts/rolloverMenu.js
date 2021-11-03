8789bc389a16b01e1336f3278b88e88a24f286a8
Emma Rutherfurd <emma@rutherfurd.no>
2021-11-03 10:09:03 +0100

    ja

--- scripts/rolloverMenu.js
+++ scripts/rolloverMenu.js
@@ -1,3 +1,11 @@
+/*
+
+https://www.w3schools.com/howto/howto_css_dropdown.asp
+
+https://blog.hubspot.com/website/html-dropdown
+https://www.javatpoint.com/how-to-create-dropdown-list-using-javascript
+*/
+
 function myFunction() {
   var x = document.getElementById("navbar");
   if (x.className === "menu") {
