d135e5bb460b22e26838cab4bb0be00e7aedb17b
Juliane Bencsik <julianeben@outlook.com>
2021-11-04 21:55:33 +0100

    PushIt

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,36 +1,33 @@
 class Footer extends HTMLElement {
-    constructor() {
-      super();
-    }
+  constructor() {
+    super();
+  }
   
     connectedCallback() {
       this.innerHTML = `
-
-
       
-		<footer> <!-- FOOTER -->
+		  <footer> <!-- FOOTER -->
 
         <div class="contactInfo"> <!-- CONTACT INFO -->
-            <p>Idiomas Por Idiotas
-            <br>mail@outlook.com
-            <br>+47 123 45 678</p>
+          <p>Idiomas Por Idiotas
+          <br>mail@outlook.com
+          <br>+47 123 45 678</p>
         </div>
 
         <div class="socialMedia"> <!-- SOCIAL MEDIA ICON / LINKS -->
-            <a href="http://www.facebook.com"> <img src="images/faceBook.svg"> </a>
-            <a href="http://www.instagram.com"> <img src="images/instaGram.svg"> </a>
-            <a href="mailto:mail@outlook.com"> <img src="images/mail.svg" target="_blank"> </a>
+          <a href="http://www.facebook.com"> <img src="images/faceBook.svg"> </a>
+          <a href="http://www.instagram.com"> <img src="images/instaGram.svg"> </a>
+          <a href="mailto:mail@outlook.com"> <img src="images/mail.svg" target="_blank"> </a>
         </div>
 
         <div class="pageLinks"> <!-- LINK TO PAGES -->
-            <a href="http://about.html">Contact us</a>
-            <a href="http://courses.html">Join a physical course!</a>
+          <a href="http://about.html">Contact us</a>
+          <a href="http://courses.html">Join a physical course!</a>
         </div>
         
-    </footer>
-    
+      </footer>
       `;
     }
   }
   
-  customElements.define('footer-component', Footer);
+customElements.define('footer-component', Footer);
--- scripts/header.js
+++ scripts/header.js
@@ -1,32 +1,26 @@
 class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
+  constructor() {
+    super();
+  }
   
     connectedCallback() {
       this.innerHTML = `
-    
 
+      <header> <!-- NAVBAR -->
 
-    <header> <!-- NAVBAR -->
+        <a class="Logo" href="home.html"><img src="images/logo.svg" height="37"></a> <!-- LOGO / HOME -->
 
-      <a class="Logo" href="home.html"><img src="images/logo.svg" height="37"></a> <!-- LOGO / HOME -->
-
-      <ul> <!-- NAVBAR OPTIONS -->
-          <li><a href="practice/practiceHome.html" id="practice">PRACTICE</a></li>
-          <li><a href="test/testHome" id="test">TEST</a></li>
+        <ul> <!-- NAVBAR OPTIONS -->
+          <li><a href="practiceHome.html" id="practice">PRACTICE</a></li>
+          <li><a href="testHome.html" id="test">TEST</a></li>
           <li><a href="courses.html" id="courses">COURSES</a></li>
           <li><a href="about.html" id="about">ABOUT US</a></li>
           <li style="line-height: 28px;">EN ▼</li>
-      </ul>
-      
-    </header>
-      
-    
+        </ul>
+        
+      </header>
       `;
     }
-  }
+  } 
   
-  customElements.define('header-component', Header);
-
-//https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
+customElements.define('header-component', Header);
