087d26722605fdc5430ade8366b327a990588fa6
Juliane Bencsik <julianeben@outlook.com>
2021-11-10 15:54:25 +0100

    endringer Juliane

--- scripts/imageCarouselMarket.js
+++ scripts/imageCarouselMarket.js
@@ -1,10 +1,10 @@
 
-
 /*This JavaScript file will make the image cards rotate like a carousel.*/
 
 const im = ["images/iconEgg.png", "images/iconBanana.png", "images/iconHen.png", "images/iconCarrot.png","images/iconSeller.png","images/iconBread.png","images/iconCash.png","images/iconFish.png","images/iconApple.png","images/iconDog.png","images/iconBlue.png"];
 let prev = document.getElementById("previousImg");
 let curr = document.getElementById("currentImg");
+let currDiv = document.getElementById("currDiv");
 let next = document.getElementById("nextImg");
 const nextButtonCarousel = document.getElementById("nextButton"); // endret fra nextButton til nextButtonCarousel
 let counterCarousel = 0;                                          // er endret fra "counter" til "counterCarousel"
@@ -23,11 +23,12 @@ function imageCarousel(){
     if (counterCarousel == (imLength-2)){
       next.style.visibility = "hidden";
     }
+
     else if(counterCarousel==imLength-1){
       prev.style.display = "none";
       curr.style.display = "none";
       next.style.display = "none";
-       
+      currDiv.style.display = "none";
     }
 }
 
